4253349d618d84dc50052f88c488f4fc08ed88fe
b1gd3vd0g <devin@bigdevdog.com>
2024-10-03 11:12:31 -0700

    Configure cors and express.json

--- template-ms/src/app.js
+++ template-ms/src/app.js
@@ -1,4 +1,12 @@
-const app = require('express')();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 
 const router = require('./router');
 app.use('/', router);
