b9f5e9316c5403eeecc0d080431b43a5f6aaa544
b1gd3vd0g <devin@bigdevdog.com>
2024-10-04 11:14:04 -0700

    create and export a router with a basic read and write method built in. (These will need to be planned out better once an API is figured out.)

--- template-ms/src/router.js
+++ template-ms/src/router.js
@@ -1,9 +1,29 @@
+const Template = require('./mongodb/model');
 const secrets = require('./secrets');
 
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    return res.status(200).json({ message: secrets.MESSAGE });
+    try {
+        const docs = await Template.find({});
+        return res.status(200).json({ docs: docs });
+    } catch (err) {
+        return res.status(500).json({ err: err });
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const defaultdoc = new Template({
+            template: 'You are a {noun}.',
+            author: 'b1gd3vd0g',
+            title: 'example'
+        });
+        const saveddoc = await defaultdoc.save();
+        return res.status(200).json({ message: 'defaultdoc created', doc: saveddoc })
+    } catch (err) {
+        return res.status(500).json({ err: err });
+    }
 })
 
 module.exports = router;
