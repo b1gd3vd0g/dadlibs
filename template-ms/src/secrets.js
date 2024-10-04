06883d8b899616e3235fe70f85f59afbe17d8e87
b1gd3vd0g <devin@bigdevdog.com>
2024-10-04 11:11:58 -0700

    Added separate .env file support for .env.production and .env.development

--- template-ms/src/secrets.js
+++ template-ms/src/secrets.js
@@ -4,4 +4,12 @@ require('dotenv').config({
     processEnv: secrets
 });
 
+require('dotenv').config({
+    processEnv: secrets,
+    path: `.env.${secrets.MODE}`
+});
+
+console.log('LOGGING SECRETS');
+console.log(secrets);
+
 module.exports = secrets;
